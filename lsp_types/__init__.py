3d85221b138c1ac5c7913766b915f7748964471d
Mazyad Alabduljaleel <mazjaleel@gmail.com>
2025-11-28 19:55:59 +0300

    add lib version support

--- lsp_types/__init__.py
+++ lsp_types/__init__.py
@@ -1,3 +1,10 @@
 from .requests import *  # noqa: F401, F403
 from .session import *  # noqa: F401, F403
 from .types import *  # noqa: F401, F403
+
+import importlib.metadata
+
+try:
+    __version__ = importlib.metadata.version("lsp-types")
+except Exception:
+    __version__ = "0.0.0"  # Fallback for development
